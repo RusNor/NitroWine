cb00eaf3255b890e0c16499589cc86d0a5c095ea
RusNor <85447162+RusNor@users.noreply.github.com>
2023-06-03 21:54:45 +0600

    Add files via upload

--- data/scripts/start.sh
+++ data/scripts/start.sh
@@ -2,20 +2,20 @@
 
 . "$(dirname $(readlink -f "$0"))/runlib"
 
-unset LD_LIBRARY_PATH
-
 export LAUNCH_GAME="   LAUNCH  GAME"
 export CREATE_SHORTCUT="   CREATE  SHORTCUT"
-export WINEFILE="   FILE  EXPLORER"
-export WINECFG="   WINE  SETTINGS"
+export WINEFILE="   EXPLORER"
+export WINECFG="   WINECFG"
+export REGEDIT="   REGEDIT"
+export UNINSTALLER="   UNINSTALLER"
 export CLEAR_PFX="   CLEAR  PREFIX"
 export WINETRICKS="   WINETRICKS"
 export STOP="   STOP"
-export SETTINGS="   SETTINGS"
+export SETTINGS="   GLOBAL  SETTINGS"
 export ICON="${WINE_PATH}/data/img/w.png"
 
 xsd=`zenity --title "$NW_VER" --text "" --window-icon="$ICON" --list --hide-header --width=270 \
---height=320 --column "" "${LAUNCH_GAME}" "${CREATE_SHORTCUT}" "${WINEFILE}" "${WINECFG}" "${CLEAR_PFX}" "${WINETRICKS}" "${STOP}" "${SETTINGS}" `
+--height=320 --column "" "${LAUNCH_GAME}" "${CREATE_SHORTCUT}" "${WINEFILE}" "${WINECFG}" "${REGEDIT}" "${UNINSTALLER}" "${CLEAR_PFX}" "${WINETRICKS}" "${STOP}" "${SETTINGS}" `
 
 if [ $? -eq 1 ]; then
     exit 1
@@ -33,6 +33,12 @@ case $xsd in
 
     "${WINECFG}")
     WINECFG "$@" ;;
+
+    "${REGEDIT}")
+    REGEDIT "$@" ;;
+
+    "${UNINSTALLER}")
+    UNINSTALLER "$@" ;;
 
     "${CLEAR_PFX}")
     CLEAR_PFX "$@" ;;
